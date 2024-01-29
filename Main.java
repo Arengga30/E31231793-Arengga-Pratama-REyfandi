5b474cd58ba25399c81c9e356bb9e60f3e0b86ad
Arengga30 <renggalaviosa78@gmail.com>
2024-01-29 15:15:16 +0700

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package no6;
+
+/**
+ *
+ * @author aarem
+ */
+public class Main {
+    public static void main(String[] args) {
+
+ int expression = 5;
+
+ switch(expression) {
+
+ case 2:
+ System.out.println("Small Size");
+ break;
+ case 3:
+ System.out.println("Large Size");
+ break;
+
+ // default case
+ default:
+ System.out.println("Unknown Size");
+ }
+ }
+} 
